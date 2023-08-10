e1cebbac1bf29abfd321f9690544981fca5a2847
Anilkokkul <anilkokkul1997@gmail.com>
2023-08-10 11:34:23 +0530

    added all files

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
- const express = require("express")
+const express = require("express")
+const fs = require("fs");
+const path = require("path");
 
- const app  = express();
+const app = express();
+const port = 5000
 
- app.get("/",(req,res)=>{
-    res.send("Hello World")
+app.use(express.json());
+
+//API to create file with current timestamp content and file name
+app.post("/createfile", (req, res) => {
+   const currentDate = new Date();
+   const fileName = `${currentDate.toISOString().replace(/[:.]/g, "-")}.txt`
+   const fileContent = `Current Timestamp: ${currentDate.getTime()}`;
+
+   fs.writeFile(`Files/${fileName}`, fileContent, (err, text)=> {
+      if (err) {
+         console.log(err);
+         return res.status(500).send("Error while creating file");
+      }
+      console.log('File created Successfully');
+      res.status(201).send({
+         massage: 'File created Successfully',
+         data: text
+      })
+   })
+
  })
 
- app.listen(8001,()=>{
-    console.log("server is started on 8001")
- })
+//API to retrive the all text files in the folder
+app.get("/getFiles", (req, res) => {
+
+   fs.readdir('Files', (err, files) => {
+      console.log(files)
+      if (err) {
+         console.log(err);
+         return res.status(500).send("Error while retrieving file");
+      }
+      console.log('Successfully retrieved files');
+      res.send({
+         massage: "Successfully retrieved files",
+         data: files
+      })
+   });
+
+});
+
+
+app.listen(port, () => {
+   console.log(`server is started on ${port}`)
+})
