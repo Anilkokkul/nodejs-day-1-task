2c8f3711a66345fe55e1370d4c1277a1504c9237
Anilkokkul <anilkokkul1997@gmail.com>
2023-08-11 15:39:22 +0530

    added routes and controllers

--- index.js
+++ index.js
@@ -1,50 +1,16 @@
 const express = require("express")
-const fs = require("fs");
+
 const path = require("path");
+const fileRoutes  = require("./Routes/files.routes")
 
 const app = express();
 const port = 5000
 
 app.use(express.json());
-
+app.use(fileRoutes)
 //API to create file with current timestamp content and file name
-app.post("/createfile", (req, res) => {
-   const currentDate = new Date();//for file name and file content
-   console.log(currentDate);
-   const fileName = `${currentDate.toISOString().replace(/[:.]/g, "-")}.txt` //this will give the file name of date and time
-   const fileContent = `Current Timestamp: ${currentDate.getTime()}`;
-
-   fs.writeFile(`Files/${fileName}`, fileContent, (err, text)=> {
-      if (err) {
-         console.log(err);
-         return res.status(500).send("Error while creating f ile");
-      }
-      console.log('File created Successfully');
-      res.status(201).send({
-         massage: 'File created Successfully',
-         data: text
-      })
-   })
-
- })
 
 //API to retrive the all text files in the folder
-app.get("/", (req, res) => {
-
-   fs.readdir('Files', (err, files) => {
-      console.log(files)
-      if (err) {
-         console.log(err);
-         return res.status(500).send("Error while retrieving file");
-      }
-      console.log('Successfully retrieved files');
-      res.send({
-         data: files
-      })
-   });
-
-});
-
 
 app.listen(port, () => {
    console.log(`server is started on ${port}`)
